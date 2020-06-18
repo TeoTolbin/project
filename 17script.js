66803337655b196cf6c27feab8478f82844d5be0
Alexey <murdok_X@mail.ru>
2020-06-19 01:04:39 +0300

    Function and properties

---
+++ 17script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const str = "teSt";
+const arr = [1, 2, 3];
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello World";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "21.2px";
+//console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
